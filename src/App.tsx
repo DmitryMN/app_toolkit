425140812b379a1c99b58170f156988ef456c450
MalenkovDN <m18dm.dm@gmail.com>
2022-08-05 15:23:29 +0300

    create actionCreators and refactoring userSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { useAppSelector } from './hooks/redux';
+import { useAppDispatch } from './hooks/redux';
+import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
+  const count = useAppSelector(state => state.userReducer.count);
+  const dispatch = useAppDispatch();
+
   return (
     <div className="App">
-      React Toolkit
+      <div>count {count}</div>
+      <button>+</button>
     </div>
   );
 }
