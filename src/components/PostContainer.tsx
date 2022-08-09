3a8cd7128012e6a170ca8806673fe6dca326ed34
MalenkovDN <m18dm.dm@gmail.com>
2022-08-09 13:07:10 +0300

    add json server and test application

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -4,13 +4,23 @@ import { IPost } from '../models/IPost';
 import { PostItem } from './PostItem';
 
 const PostContainer = () => {
-    const {data: posts, error ,isLoading} = postApi.useFetchAllPostsQuery(5);
+    const {data: posts, error ,isLoading} = postApi.useFetchAllPostsQuery(100);
+    const [createPost, {}] = postApi.useCreatePostMutation();
+    const [updatePost, {}] = postApi.useUpdatePostMutation();
+    const [removePost, {}] = postApi.useDeletePostMutation();
+
+    const handleCreate = async () => {
+        const title = prompt();
+        await createPost({title} as IPost);
+    }
+
     return (
         <div>
             <div className="post__list">
+                <button onClick={handleCreate}>Add post</button>
                 {isLoading && <h1>Идет загрузка...</h1>}
                 {error && <div>Ошибка загрузки</div>}
-                {posts && posts.map(post => <PostItem key={post.id} post={post} />)}
+                {posts && posts.map(post => <PostItem key={post.id} post={post} update={updatePost} remove={removePost} />)}
             </div>
         </div>
     )
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -3,15 +3,28 @@ import { IPost } from '../models/IPost'
 
 type PostItemType = {
   post: IPost
+  update: (post: IPost) => void
+  remove: (post: IPost) => void
 }
 
-export const PostItem: React.FC<PostItemType> = ({ post }) => {
+export const PostItem: React.FC<PostItemType> = ({ post, update, remove }) => {
+
+  const handleUpdate = (event: React.MouseEvent) => {
+    const title = prompt() || "";
+    update({...post, title});
+  }
+
+  const handleRemove = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    remove(post);
+  }
+
   return (
-    <div className='post'>
+    <div className='post' style={{border: "1px solid black"}} onClick={handleUpdate}>
       <p>
         {post.id}: {post.title}
       </p>
-      <button>delete</button>
+      <button onClick={handleRemove}>delete</button>
     </div>
   )
 }
